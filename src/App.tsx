427251bb1663df9f98b408118b2458e7d9f9bfb3
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-06-03 14:14:36 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header.tsx'
+import goalsImg from './assets/goals.jpg'
+
+function App() {
+  return (
+    <main>
+      <Header image={{ src: goalsImg, alt: 'A list of goals' }} />
+    </main>
+  )
+}
+
+export default App
